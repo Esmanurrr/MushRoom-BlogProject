0674734dc92a3ec0c7b6b8cc062d54f8b4932226
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-12-26 21:37:51 +0300

    [Fix] Optimization

---
+++ Presentation/MushRoom.API/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MushRoom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
