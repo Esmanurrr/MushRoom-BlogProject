0de81aced979afd8b34b0ffd44ea6fb5f7e6648a
Bengisoo <zehrabengisu8@gmail.com>
2023-12-30 19:38:16 +0300

    [Feat] Added repository base

---
+++ Core/MushRoom.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using MushRoom.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MushRoom.Application.Repositories
+{
+    public interface IReadRepository<T, TKey>: IRepository<T, TKey> where T : EntityBase<TKey>
+    {
+        List<T> GetAll();
+        T GetById(TKey id);
+    }
+}
---
+++ Core/MushRoom.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,15 @@
+﻿using MushRoom.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Repositories
+{
+    public interface IWriteRepository<T, TKey>: IRepository<T, TKey> where T: EntityBase<TKey>
+    {
+        void Add(T entity);
+        void Delete(string id);
+    }
+}
