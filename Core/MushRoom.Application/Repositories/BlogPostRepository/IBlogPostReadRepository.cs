4afce00ab7ee8824eccb558887e72a6909107863
Bengisoo <zehrabengisu8@gmail.com>
2023-12-30 19:36:44 +0300

    [Feat] Added IEntity repositories

---
+++ Core/MushRoom.Application/Repositories/BlogPostRepository/IBlogPostReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using MushRoom.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MushRoom.Domain.Common;
+using MushRoom.Domain.Entities;
+
+namespace MushRoom.Application.Repositories.BlogPostRepository
+{
+    public interface IBlogPostReadRepository: IReadRepository<BlogPost, Guid>
+    {
+
+    }
+}
---
+++ Core/MushRoom.Application/Repositories/BlogPostRepository/IBlogPostWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using MushRoom.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Repositories.BlogPostRepository
+{
+    public interface IBlogPostWriteRepository: IWriteRepository<BlogPost, Guid>
+    {
+    }
+}
