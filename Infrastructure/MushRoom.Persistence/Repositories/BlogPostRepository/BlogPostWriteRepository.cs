78ce41e6c2b0dd9bea92f9855b4159f9dfdaa0c3
Bengisoo <zehrabengisu8@gmail.com>
2023-12-30 19:38:37 +0300

    [Feat] Added BlogPost Repository

---
+++ Infrastructure/MushRoom.Persistence/Repositories/BlogPostRepository/BlogPostWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Persistence.Repositories.BlogPostRepository
+{
+    public class BlogPostWriteRepository
+    {
+    }
+}
