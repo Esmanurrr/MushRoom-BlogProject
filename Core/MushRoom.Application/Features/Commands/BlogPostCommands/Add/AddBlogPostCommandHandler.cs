6333fb376337900fdaf321926b218bb2bc4469f8
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2023-12-31 00:31:23 +0300

    [Feat] Added Queries&Commands
    BlogPostQueries and BlogPostCommands

---
+++ Core/MushRoom.Application/Features/Commands/BlogPostCommands/Add/AddBlogPostCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using MushRoom.Application.Repositories.BlogPostRepository;
+using MushRoom.Domain.Entities;
+using MushRoom.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.BlogPostCommands.Add
+{
+    public class AddBlogPostCommandHandler : IRequestHandler<AddBlogPostCommandRequest, AddBlogPostCommandResponse>
+    {
+        private readonly IBlogPostWriteRepository _blogPostWriteRepository;
+
+        public AddBlogPostCommandHandler(IBlogPostWriteRepository blogPostWriteRepository)
+        {
+            _blogPostWriteRepository = blogPostWriteRepository;
+        }
+
+        public Task<AddBlogPostCommandResponse> Handle(AddBlogPostCommandRequest request, CancellationToken cancellationToken)
+        {
+            var blogPost = new BlogPost()
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                Content = request.Content,
+                //User = request.User,
+            };
+
+          _blogPostWriteRepository.Add(blogPost);
+          //_blogPostWriteRepository.SaveChanges();
+
+            return Task.FromResult(new AddBlogPostCommandResponse()); 
+        }
+    }
+}
---
+++ Core/MushRoom.Application/Features/Commands/BlogPostCommands/Add/AddBlogPostCommandRequest.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MushRoom.Application.Features.Commands.BlogPostCommands.Add;
+using MushRoom.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.BlogPostCommands.Add;
+
+public class AddBlogPostCommandRequest : IRequest<AddBlogPostCommandResponse>
+{
+    public string Title {  get; set; }
+    public string Content { get; set; }
+
+   // public User User { get; set; }
+  
+
+}
