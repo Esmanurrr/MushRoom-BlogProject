a203ad3f88318a866a9435dec87c3bb0d87eec85
Bengisoo <zehrabengisu8@gmail.com>
2024-01-01 20:36:56 +0300

    [Update] persistence

--- Infrastructure/MushRoom.Persistence/Contexts/MushRoomDbContext.cs
+++ Infrastructure/MushRoom.Persistence/Contexts/MushRoomDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MushRoom.Domain.Common;
 using MushRoom.Domain.Entities;
 using MushRoom.Domain.Identity;
 using System;
@@ -15,11 +16,11 @@ namespace MushRoom.Persistence.Contexts
 {
     public class MushRoomDbContext : IdentityDbContext<User, Role, Guid>
     {
-        DbSet<BlogPost> BlogPosts { get; set; }
-        DbSet<Comment> Comments { get; set; }
-        DbSet<Like> Likes { get; set; }
-        DbSet<Tag> Tags { get; set; }
-        DbSet<BlogPostTag> BlogPostTags { get; set; }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<BlogPostTag> BlogPostTags { get; set; }
         public MushRoomDbContext(DbContextOptions options) : base(options)
         {
         }
@@ -29,5 +30,31 @@ namespace MushRoom.Persistence.Contexts
 
             base.OnModelCreating(modelBuilder);
         }
+
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker.Entries();
+
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                    ((ICreatedOn)entry.Entity).CreatedOn = DateTime.UtcNow;
+
+                if (entry.State == EntityState.Modified)
+                    ((IModifiedOn)entry.Entity).ModifiedOn = DateTime.UtcNow;
+
+            }
+
+            return base.SaveChanges();
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseNpgsql(Configurations.GetString("ConnectionStrings:PostgreSQL"));
+        //}
+
+
+
+
     }
 }
