8e90d65476758b675f2718763663d0729440110f
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-01-01 22:43:10 +0300

    [Feature] Added Delete Commands

---
+++ Core/MushRoom.Application/Features/Commands/BlogPostCommands/Delete/DeleteBlogPostCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MushRoom.Application.Repositories.BlogPostRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.BlogPostCommands.Delete
+{
+    public class DeleteBlogPostCommandHandler : IRequestHandler<DeleteBlogPostCommandRequest, DeleteBlogPostCommandResponse>
+    {
+        private readonly IBlogPostWriteRepository _blogPostWriteRepository;
+
+        public DeleteBlogPostCommandHandler(IBlogPostWriteRepository blogPostWriteRepository)
+        {
+            _blogPostWriteRepository = blogPostWriteRepository;
+        }
+
+        public async Task<DeleteBlogPostCommandResponse> Handle(DeleteBlogPostCommandRequest request, CancellationToken cancellationToken)
+        {
+            _blogPostWriteRepository.Delete(request.Id);
+            _blogPostWriteRepository.SaveChanges();
+            return new DeleteBlogPostCommandResponse();
+
+        }
+    }
+}
---
+++ Core/MushRoom.Application/Features/Commands/BlogPostCommands/Delete/DeleteBlogPostCommandRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.BlogPostCommands.Delete
+{
+    public class DeleteBlogPostCommandRequest : IRequest<DeleteBlogPostCommandResponse>
+    {
+        public Guid Id { get; set; }
+    }
+}
