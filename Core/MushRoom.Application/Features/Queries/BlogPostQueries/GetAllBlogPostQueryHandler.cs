68fe6f549cd0885b55939b605b7c023a5b844776
Bengisoo <zehrabengisu8@gmail.com>
2024-01-01 20:35:11 +0300

    [Add] Added GetAllBlogPostQuery handler, request and reponse

--- Core/MushRoom.Application/Features/Queries/BlogPostQueries/GetAllBlogPostQueryHandler.cs
+++ Core/MushRoom.Application/Features/Queries/BlogPostQueries/GetAllBlogPostQueryHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MediatR;
+using MediatR.NotificationPublishers;
+using MushRoom.Application.Repositories.BlogPostRepository;
+using MushRoom.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,39 @@ using System.Threading.Tasks;
 
 namespace MushRoom.Application.Features.Queries.BlogPostQueries
 {
-    public class GetAllBlogPostQueryHandler
+    public class GetAllBlogPostQueryHandler : 
+        IRequestHandler<GetAllBlogPostQueryRequest, List<GetAllBlogPostQueryResponse>>
     {
+        private readonly IBlogPostReadRepository _blogPostReadRepository;
+
+        public GetAllBlogPostQueryHandler(IBlogPostReadRepository blogPostReadRepository)
+        {
+            _blogPostReadRepository = blogPostReadRepository;
+        }
+
+        public async Task<List<GetAllBlogPostQueryResponse>> Handle(GetAllBlogPostQueryRequest request, CancellationToken cancellationToken)
+        {
+            List<BlogPost> posts = _blogPostReadRepository.GetAll();
+
+            List<GetAllBlogPostQueryResponse> allPosts= new List<GetAllBlogPostQueryResponse>();
+
+            foreach (var post in posts)
+            {
+                GetAllBlogPostQueryResponse obj = new()
+                {
+                    Content = post.Content,
+                    Title = post.Title,
+                    Id = post.Id,
+                    Tags = post.BlogPostTags,
+                    Username = post.User.UserName
+                };
+                allPosts.Add(obj);
+
+            }
+
+
+            return allPosts;
+        }
+
     }
 }
--- Core/MushRoom.Application/Features/Queries/BlogPostQueries/GetAllBlogPostQueryResponse.cs
+++ Core/MushRoom.Application/Features/Queries/BlogPostQueries/GetAllBlogPostQueryResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MushRoom.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace MushRoom.Application.Features.Queries.BlogPostQueries
 {
     public class GetAllBlogPostQueryResponse
     {
+        //List<BlogPost> blogPosts;
+        //public BlogPost post {  get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Username { get; set; }
+        public ICollection<BlogPostTag> Tags { get; set; }
     }
 }
