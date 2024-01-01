14d3413e5d81fbece2bd5423d7d912450fc9548e
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2024-01-01 21:36:57 +0300

    [Feat] Updated GetAllBlogPost

---
+++ Core/MushRoom.Application/Features/Queries/BlogPostQueries/GetAll/GetAllBlogPostQueryRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Queries.BlogPostQueries.GetAll
+{
+    public class GetAllBlogPostQueryRequest : IRequest<List<GetAllBlogPostQueryResponse>>
+    {
+    }
+}
---
+++ Core/MushRoom.Application/Features/Queries/BlogPostQueries/GetAll/GetAllBlogPostQueryResponse.cs
@@ -0,0 +1,20 @@
+﻿using MushRoom.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Queries.BlogPostQueries.GetAll
+{
+    public class GetAllBlogPostQueryResponse
+    {
+        //List<BlogPost> blogPosts;
+        //public BlogPost post {  get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Username { get; set; }
+        public ICollection<BlogPostTag> Tags { get; set; }
+    }
+}
