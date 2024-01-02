a6a99ccc745ecda35975ddfc535e75e0637c042e
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2024-01-02 17:13:09 +0300

    [Feat] Added AddCommentCommand

---
+++ Core/MushRoom.Application/Features/Commands/CommentCommands/Add/AddCommentCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using MushRoom.Application.Repositories.BlogPostRepository;
+using MushRoom.Application.Repositories.CommentRepository;
+using MushRoom.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.CommentCommands.Add
+{
+    public class AddCommentCommandHandler : IRequestHandler<AddCommentCommandRequest, AddCommentCommandResponse>
+    {
+        private readonly ICommentWriteRepository _commentWriteRepository;
+
+        public AddCommentCommandHandler(ICommentWriteRepository commentWriteRepository)
+        {
+            _commentWriteRepository = commentWriteRepository;
+        }
+
+        public async Task<AddCommentCommandResponse> Handle(AddCommentCommandRequest request, CancellationToken cancellationToken)
+        {
+            // Yorum ekleme işlemi
+            var comment = new Comment
+            {
+                Content = request.Content,
+                UserId = request.UserId,
+                BlogPostId = request.BlogPostId
+            };
+
+            _commentWriteRepository.Add(comment);
+            _commentWriteRepository.SaveChanges();
+
+            // Eklendiyse yanıt oluşturma
+            var response = new AddCommentCommandResponse
+            {
+                IsSuccess = comment.Id != Guid.Empty
+            };
+
+            return response;
+        }
+
+
+    }
+}
+
---
+++ Core/MushRoom.Application/Features/Commands/CommentCommands/Add/AddCommentCommandRequest.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.CommentCommands.Add
+{
+    public class AddCommentCommandRequest : IRequest<AddCommentCommandResponse>
+    {
+        public string Content { get; set; }
+        public Guid UserId { get; set; }
+        public Guid BlogPostId { get; set; }
+
+    }
+}
