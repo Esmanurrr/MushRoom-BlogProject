f3d6593fdd83f7accd3fc6224341b3e390c51e04
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2024-01-02 17:13:24 +0300

    [Feat] Added DeleteCommentCommand

---
+++ Core/MushRoom.Application/Features/Commands/CommentCommands/Delete/DeleteCommentCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using MushRoom.Application.Features.Commands.BlogPostCommands.Delete;
+using MushRoom.Application.Repositories.CommentRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.CommentCommands.Delete
+{
+    public class DeleteCommentCommandHandler : IRequestHandler<DeleteCommentCommandRequest, DeleteCommentCommandResponse>
+    {
+        private readonly ICommentWriteRepository _commentWriteRepository;
+
+        public DeleteCommentCommandHandler(ICommentWriteRepository commentWriteRepository)
+        {
+            _commentWriteRepository = commentWriteRepository;
+        }
+
+        public async Task<DeleteCommentCommandResponse> Handle(DeleteCommentCommandRequest request, CancellationToken cancellationToken)
+        {
+            // CommentId'si üzerinden yorumu bul ve sil
+            _commentWriteRepository.Delete(request.CommentId);
+            _commentWriteRepository.SaveChanges();
+            var response = new DeleteCommentCommandResponse
+            {
+                IsSuccess = true,
+                DeletedCommentId = request.CommentId // Silinen yorumun kimliği veya diğer bilgiler...
+            };
+
+            return response;
+
+
+
+        }
+    }
+}
---
+++ Core/MushRoom.Application/Features/Commands/CommentCommands/Delete/DeleteCommentCommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MushRoom.Application.Features.Commands.BlogPostCommands.Delete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.CommentCommands.Delete
+{
+    public class DeleteCommentCommandRequest : IRequest<DeleteCommentCommandResponse>
+    {
+        public Guid CommentId { get; set; }
+    }
+}
