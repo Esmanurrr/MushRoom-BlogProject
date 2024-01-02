3bec8d7d83d6e523235d4f036a334f3988999816
Bengisoo <zehrabengisu8@gmail.com>
2024-01-02 23:34:14 +0300

    [Add] Added Add Command

---
+++ Core/MushRoom.Application/Features/Commands/TagCommands/Add/AddTagCommandResponse.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.TagCommands.Add
+{
+    public class AddTagCommandResponse
+    {
+        public bool IsSuccess { get; set; }
+    }
+}
