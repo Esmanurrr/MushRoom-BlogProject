21912b31d7859285728a02417c18b418575a6dae
Bengisoo <zehrabengisu8@gmail.com>
2024-01-02 23:36:23 +0300

    [Add] Added Get By Id Query for Tag

---
+++ Core/MushRoom.Application/Features/Queries/TagQueries/GetById/GetByIdTagQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using MushRoom.Application.Repositories.TagRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Queries.TagQueries.GetById
+{
+    public class GetByIdTagQueryHandler : IRequestHandler<GetByIdTagQueryRequest<Guid>, GetByIdTagQueryResponse>
+    {
+        private readonly ITagReadRepository _tagReadRepository;
+
+        public GetByIdTagQueryHandler(ITagReadRepository tagReadRepository)
+        {
+            _tagReadRepository = tagReadRepository;
+        }
+
+        public async Task<GetByIdTagQueryResponse> Handle(GetByIdTagQueryRequest<Guid> request, CancellationToken cancellationToken)
+        {
+            var tag =_tagReadRepository.GetById(request.Id);
+            return new() { Name = tag.Name };
+        }
+    }
+}
