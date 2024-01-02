2f3d7c3f8cc13b45bc7ab206f12b19bc1fdd10e9
Bengisoo <zehrabengisu8@gmail.com>
2024-01-02 23:34:35 +0300

    [Add] Added Delete Command

---
+++ Core/MushRoom.Application/Features/Commands/TagCommands/Delete/DeleteTagCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MushRoom.Application.Repositories.TagRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MushRoom.Application.Features.Commands.TagCommands.Delete
+{
+    public class DeleteTagCommandHandler : IRequestHandler<DeleteTagCommandRequest, DeleteTagCommandResponse>
+    {
+        private readonly ITagWriteRepository _tagWriteRepository;
+
+        public DeleteTagCommandHandler(ITagWriteRepository tagWriteRepository)
+        {
+            _tagWriteRepository = tagWriteRepository;
+        }
+
+        public async Task<DeleteTagCommandResponse> Handle(DeleteTagCommandRequest request, CancellationToken cancellationToken)
+        {
+            _tagWriteRepository.Delete(request.Id);
+            _tagWriteRepository.SaveChanges();
+            return new DeleteTagCommandResponse();
+        }
+    }
+}
