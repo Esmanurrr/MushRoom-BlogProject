dd7335125d21dae5eb80d9e7bc5cbde088541e73
Cengizhan <civelekc18@itu.edu.tr>
2024-01-03 23:33:53 +0300

    [Feat] Added model classes.

---
+++ Core/MushRoom.Application/Models/RegistrationModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MushRoom.API.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
+
