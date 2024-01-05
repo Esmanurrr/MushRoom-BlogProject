17bdd24610f3e99286b8a32a80c44575ee3679c6
Cengizhan <civelekc18@itu.edu.tr>
2024-01-05 12:21:36 +0300

    [Fix] Cleaned data annotation.

--- Core/MushRoom.Application/Models/LoginModel.cs
+++ Core/MushRoom.Application/Models/LoginModel.cs
@@ -3,11 +3,8 @@
 namespace MushRoom.API.Models
 {
     public class LoginModel
-    {
-        [Required(ErrorMessage = "User Name is required")]
-        public string? Username { get; set; }
-
-        [Required(ErrorMessage = "Password is required")]
-        public string? Password { get; set; }
+    {        
+        public string Username { get; set; }      
+        public string Password { get; set; }
     }
 }
--- Core/MushRoom.Application/Models/RegistrationModel.cs
+++ Core/MushRoom.Application/Models/RegistrationModel.cs
@@ -3,18 +3,11 @@
 namespace MushRoom.API.Models
 {
     public class RegistrationModel
-    {
-        [Required(ErrorMessage = "User Name is required")]
-        public string Username { get; set; }
-        [Required(ErrorMessage = "Name is required")]
+    {      
+        public string Username { get; set; }  
         public string FirstName { get; set; }
         public string SurName { get; set; }
-
-        [EmailAddress]
-        [Required(ErrorMessage = "Email is required")]
-        public string Email { get; set; }
-
-        [Required(ErrorMessage = "Password is required")]
+        public string Email { get; set; }     
         public string Password { get; set; }
     }
 }
